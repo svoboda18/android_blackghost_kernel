0082f13b7396673248d3b40be71902f50e2c08fb
svoboda18 <dev.svoboda18@gmail.com>
2019-05-30 03:34:37 +0000

    x5: touch: fix damn typo!

--- drivers/input/touchscreen/mediatek/ft5x0x/ft5x0x_util.h
+++ drivers/input/touchscreen/mediatek/ft5x0x/ft5x0x_util.h
@@ -45,7 +45,7 @@ static void ft5x0x_power(bool bOn)
 	if (!tpd->reg)
 	{
 		tpd->reg = regulator_get(tpd->tpd_dev, "vtouch");
-		retval = regulator_set_voltage(tpd->reg, 2400000, 200000);
+		retval = regulator_set_voltage(tpd->reg, 2400000, 2400000);
 	}
 
 	ft5x0x_set_rst(false, 20);
