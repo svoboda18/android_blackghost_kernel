995de23c82c9f44c31c06095349edd04b914947f
svoboda18 <ruspam18@gmail.com>
2019-07-14 18:18:34 +0000

    x5: boots on twrp! * bootloop fixed, credits to @svoboda18. * lcm work, touch work.

--- scripts/drvgen/drvgen.mk
+++ scripts/drvgen/drvgen.mk
@@ -70,6 +70,12 @@ $(DRVGEN_FILE_LIST): $(DRVGEN_TOOL) $(DWS_FILE) $(DRVGEN_FIG) $(PROJ_DTS_FILES)
 		fi \
 	done
 
+
+dtbo_check: 
+	echo "Eh"
+
+ifeq ($(strip $(CONFIG_MTK_DTBO_FEATURE)), y)
+
 dtbo_check: $(MAIN_DTB_NAMES) $(PROJ_DTB_NAMES)
 	for i in $(PROJ_DTB_FILES); do \
 		$(srctree)/scripts/dtc/ufdt_apply_overlay $(MAIN_DTB_FILES) $$i $$i.merge;\
@@ -92,4 +98,5 @@ $(objtree)/dtboimg.cfg: FORCE
 		rm $@.tmp; \
 	fi
 
+endif
 endif#MTK_PLATFORM
