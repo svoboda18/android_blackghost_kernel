fea2d91020a9956dc1b02125b099c992fb0daaa8
Greg Kroah-Hartman <gregkh@google.com>
2021-03-07 11:40:25 +0100

    Merge 4.9.260 into android-4.9-q
    Changes in 4.9.260
    	futex: Cleanup variable names for futex_top_waiter()
    	futex: Cleanup refcounting
    	futex: Pull rt_mutex_futex_unlock() out from under hb->lock
    	futex: Futex_unlock_pi() determinism
    	futex: Fix pi_state->owner serialization
    	futex: Fix more put_pi_state() vs. exit_pi_state_list() races
    	futex: Don't enable IRQs unconditionally in put_pi_state()
    	net: usb: qmi_wwan: support ZTE P685M modem
    	arm: kprobes: Allow to handle reentered kprobe on single-stepping
    	scripts: use pkg-config to locate libcrypto
    	scripts: set proper OpenSSL include dir also for sign-file
    	hugetlb: fix update_and_free_page contig page struct assumption
    	printk: fix deadlock when kernel panic
    	arm64: Remove redundant mov from LL/SC cmpxchg
    	arm64: Avoid redundant type conversions in xchg() and cmpxchg()
    	arm64: cmpxchg: Use "K" instead of "L" for ll/sc immediate constraint
    	arm64: Use correct ll/sc atomic constraints
    	JFS: more checks for invalid superblock
    	xfs: Fix assert failure in xfs_setattr_size()
    	smackfs: restrict bytes count in smackfs write functions
    	net: fix up truesize of cloned skb in skb_prepare_for_shift()
    	mm/hugetlb.c: fix unnecessary address expansion of pmd sharing
    	staging: fwserial: Fix error handling in fwserial_create
    	x86/reboot: Add Zotac ZBOX CI327 nano PCI reboot quirk
    	vt/consolemap: do font sum unsigned
    	wlcore: Fix command execute failure 19 for wl12xx
    	pktgen: fix misuse of BUG_ON() in pktgen_thread_worker()
    	ath10k: fix wmi mgmt tx queue full due to race condition
    	x86/build: Treat R_386_PLT32 relocation as R_386_PC32
    	Bluetooth: Fix null pointer dereference in amp_read_loc_assoc_final_data
    	staging: most: sound: add sanity check for function argument
    	media: uvcvideo: Allow entities with no pads
    	scsi: iscsi: Restrict sessions and handles to admin capabilities
    	sysfs: Add sysfs_emit and sysfs_emit_at to format sysfs output
    	scsi: iscsi: Ensure sysfs attributes are limited to PAGE_SIZE
    	scsi: iscsi: Verify lengths on passthrough PDUs
    	Xen/gnttab: handle p2m update errors on a per-slot basis
    	xen-netback: respect gnttab_map_refs()'s return value
    	zsmalloc: account the number of compacted pages correctly
    	swap: fix swapfile read/write offset
    	media: v4l: ioctl: Fix memory leak in video_usercopy
    	Linux 4.9.260
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I36c2dd54eec10fff4b30d04830dbf0c3928ec088

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1107,8 +1107,9 @@ static ssize_t mm_stat_show(struct device *dev,
 			mem_used << PAGE_SHIFT,
 			zram->limit_pages << PAGE_SHIFT,
 			max_used << PAGE_SHIFT,
-			(u64)atomic64_read(&zram->stats.zero_pages),
-			atomic_long_read(&pool_stats.pages_compacted));
+			(u64)atomic64_read(&zram->stats.same_pages),
+			atomic_long_read(&pool_stats.pages_compacted),
+			(u64)atomic64_read(&zram->stats.huge_pages));
 	up_read(&zram->init_lock);
 
 	return ret;
