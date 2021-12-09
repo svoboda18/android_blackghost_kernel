c7a63f3f3dd8d1c4a1e0dc870fd9b72b8d4984a6
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2021-12-09 19:02:15 +0100

    UPSTREAM: USB: gadget: zero allocate endpoint 0 buffers
    Under some conditions, USB gadget devices can show allocated buffer
    contents to a host.  Fix this up by zero-allocating them so that any
    extra data will all just be zeros.
    
    Reported-by: Szymon Heidrich <szymon.heidrich@gmail.com>
    Tested-by: Szymon Heidrich <szymon.heidrich@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 86ebbc11bb3f60908a51f3e41a17e3f477c2eaa3)
    Bug: 210292367
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I72b4376cd4296a8b8af0ade2d702cd420146f3aa
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/usb/gadget/composite.c
+++ drivers/usb/gadget/composite.c
@@ -2267,9 +2267,9 @@ int composite_dev_prepare(struct usb_composite_driver *composite,
 	if (!cdev->req)
 		return -ENOMEM;
 #if defined(CONFIG_64BIT) && defined(CONFIG_MTK_LM_MODE)
-	cdev->req->buf = kmalloc(USB_COMP_EP0_BUFSIZ, GFP_KERNEL | GFP_DMA);
+	cdev->req->buf = kzalloc(USB_COMP_EP0_BUFSIZ, GFP_KERNEL | GFP_DMA);
 #else
-	cdev->req->buf = kmalloc(USB_COMP_EP0_BUFSIZ, GFP_KERNEL);
+	cdev->req->buf = kzalloc(USB_COMP_EP0_BUFSIZ, GFP_KERNEL);
 #endif
 	if (!cdev->req->buf)
 		goto fail;
