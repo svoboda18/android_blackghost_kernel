ca72ba21dca7bb67dd861c37a417a676c6601d14
Greg Kroah-Hartman <gregkh@google.com>
2022-03-02 15:26:26 +0100

    Merge 4.9.304 into android-4.9-q
    Changes in 4.9.304
    	mtd: rawnand: brcmnand: Fixed incorrect sub-page ECC status
    	vhost/vsock: don't check owner in vhost_vsock_stop() while releasing
    	parisc/unaligned: Fix fldd and fstd unaligned handlers on 32-bit kernel
    	parisc/unaligned: Fix ldw() and stw() unalignment handlers
    	sr9700: sanity check for packet length
    	USB: zaurus: support another broken Zaurus
    	serial: 8250: fix error handling in of_platform_serial_probe()
    	serial: 8250: of: Fix mapped region size when using reg-offset property
    	net: __pskb_pull_tail() & pskb_carve_frag_list() drop_monitor friends
    	gso: do not skip outer ip header in case of ipip and net_failover
    	openvswitch: Fix setting ipv6 fields causing hw csum failure
    	drm/edid: Always set RGB444
    	net/mlx5e: Fix wrong return value on ioctl EEPROM query failure
    	configfs: fix a race in configfs_{,un}register_subsystem()
    	RDMA/ib_srp: Fix a deadlock
    	iio: adc: men_z188_adc: Fix a resource leak in an error handling path
    	ata: pata_hpt37x: disable primary channel on HPT371
    	Revert "USB: serial: ch341: add new Product ID for CH341A"
    	usb: gadget: rndis: add spinlock for rndis response list
    	USB: gadget: validate endpoint index for xilinx udc
    	tracefs: Set the group ownership in apply_options() not parse_options()
    	USB: serial: option: add support for DW5829e
    	USB: serial: option: add Telit LE910R1 compositions
    	usb: dwc3: gadget: Let the interrupt handler disable bottom halves.
    	xhci: Prevent futile URB re-submissions due to incorrect return value.
    	tty: n_gsm: fix encoding of control signal octet bit DV
    	tty: n_gsm: fix proper link termination after failed open
    	memblock: use kfree() to release kmalloced memblock regions
    	fget: clarify and improve __fget_files() implementation
    	Linux 4.9.304
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I2e6047b09e3b477b03005b985987d4bc9fe2d44a

--- drivers/usb/gadget/function/rndis.c
+++ drivers/usb/gadget/function/rndis.c
@@ -1236,6 +1236,7 @@ struct rndis_params *rndis_register(void (*resp_avail)(void *v), void *v)
 	params->resp_avail = resp_avail;
 	params->v = v;
 	INIT_LIST_HEAD(&params->resp_queue);
+	spin_lock_init(&params->resp_lock);
 	pr_debug("%s: configNr = %d\n", __func__, i);
 
 #ifdef CONFIG_MTK_MD_DIRECT_TETHERING_SUPPORT
@@ -1360,6 +1361,7 @@ void rndis_free_response(struct rndis_params *params, u8 *buf)
 	if (rndis_debug > 2)
 		RNDIS_DBG("\n");
 
+	spin_lock(&params->resp_lock);
 	list_for_each_entry_safe(r, n, &params->resp_queue, list) {
 		if (r->buf == buf) {
 			list_del(&r->list);
@@ -1377,6 +1379,7 @@ u8 *rndis_get_next_response(struct rndis_params *params, u32 *length)
 
 	if (!length) return NULL;
 
+	spin_lock(&params->resp_lock);
 	list_for_each_entry_safe(r, n, &params->resp_queue, list) {
 		if (!r->send) {
 			r->send = 1;
@@ -1406,7 +1409,9 @@ static rndis_resp_t *rndis_add_response(struct rndis_params *params, u32 length)
 	r->length = length;
 	r->send = 0;
 
+	spin_lock(&params->resp_lock);
 	list_add_tail(&r->list, &params->resp_queue);
+	spin_unlock(&params->resp_lock);
 	return r;
 }
 
