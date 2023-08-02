1dda94726b1535f832ee3f7fabca939656580d41
svoboda18 <dev.svoboda18@gmail.com>
2023-08-02 13:19:24 +0000

    X5: add config, dts and lcm
    Signed-off-by: svoboda18 <dev.svoboda18@gmail.com>

--- drivers/misc/mediatek/lcm/mt65xx_lcm_list.h
+++ drivers/misc/mediatek/lcm/mt65xx_lcm_list.h
@@ -16,6 +16,8 @@
 
 #include <lcm_drv.h>
 
+extern struct LCM_DRIVER hct_hx8394f_dsi_vdo_hd_cmi;
+
 extern struct LCM_DRIVER otm1282a_hd720_dsi_vdo_60hz_lcm_drv;
 extern struct LCM_DRIVER otm1282a_hd720_dsi_vdo_lcm_drv;
 extern struct LCM_DRIVER nt35595_fhd_dsi_cmd_truly_8163_lcm_drv;
