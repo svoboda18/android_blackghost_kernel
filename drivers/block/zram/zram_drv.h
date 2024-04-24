9de2ff5d372a6e24c195b54e552cc05b46c3845e
svoboda18 <dev.svoboda18@gmail.com>
2024-04-24 20:30:53 +0100

    zram: use lz4 compression method This commit part-reverts e335c89
    Signed-off-by: svoboda18 <dev.svoboda18@gmail.com>

--- drivers/block/zram/zram_drv.h
+++ drivers/block/zram/zram_drv.h
@@ -39,7 +39,7 @@
  * The lower ZRAM_FLAG_SHIFT bits is for object size (excluding header),
  * the higher bits is for zram_pageflags.
  */
-#define ZRAM_FLAG_SHIFT 16
+#define ZRAM_FLAG_SHIFT 24
 
 /* Flags for zram pages (table[page_no].flags) */
 enum zram_pageflags {
