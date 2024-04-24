6c3509956abce930adc2736bf2d52333e7fb0d6a
svoboda18 <dev.svoboda18@gmail.com>
2024-04-24 20:35:17 +0100

    selinux: ignore granted audit spam
    Signed-off-by: svoboda18 <dev.svoboda18@gmail.com>

--- security/selinux/avc.c
+++ security/selinux/avc.c
@@ -760,6 +760,9 @@ noinline int slow_avc_audit(u32 ssid, u32 tsid, u16 tclass,
 	struct common_audit_data stack_data;
 	struct selinux_audit_data sad;
 
+	if (!denied)
+		return 0;
+
 	if (!a) {
 		a = &stack_data;
 		a->type = LSM_AUDIT_DATA_NONE;
