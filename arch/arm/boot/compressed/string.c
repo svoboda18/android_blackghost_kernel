c68b9316d427f63e17524441c420709b91e564af
svoboda18 <dev.svoboda18@gmail.com>
2024-05-06 22:21:41 +0100

    ARM: use UAL assembly syntax to support LLVM_IAS
    Signed-off-by: svoboda18 <dev.svoboda18@gmail.com>

--- arch/arm/boot/compressed/string.c
+++ arch/arm/boot/compressed/string.c
@@ -129,8 +129,3 @@ void *memset(void *s, int c, size_t count)
 		*xs++ = c;
 	return s;
 }
-
-void __memzero(void *s, size_t count)
-{
-	memset(s, 0, count);
-}
--- arch/arm/include/asm/string.h
+++ arch/arm/include/asm/string.h
@@ -24,18 +24,4 @@ extern void * memchr(const void *, int, __kernel_size_t);
 #define __HAVE_ARCH_MEMSET
 extern void * memset(void *, int, __kernel_size_t);
 
-extern void __memzero(void *ptr, __kernel_size_t n);
-
-#define memset(p,v,n)							\
-	({								\
-	 	void *__p = (p); size_t __n = n;			\
-		if ((__n) != 0) {					\
-			if (__builtin_constant_p((v)) && (v) == 0)	\
-				__memzero((__p),(__n));			\
-			else						\
-				memset((__p),(v),(__n));		\
-		}							\
-		(__p);							\
-	})
-
 #endif
